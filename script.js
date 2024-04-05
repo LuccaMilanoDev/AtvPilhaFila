5839e3ef219a6997c74bfe209a87468ea10f0171
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-04-05 18:20:48 -0300

    finalizando tarefa simples

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-const vetor = [];
-const number = document.getElementById("number").value
-function add(){
-   vetor.push[2]
-   console.log(vetor[0])
+let vetor = [];
+let number = document.querySelector("#number");
+let isPilhaSelecionado = false;
+let isFilaSelecionado = false;
+
+function add() {
+  if (isPilhaSelecionado) {
+    vetor.push(number.value);
+    console.log(vetor);
+    console.log(number.value);
+  } else if(isFilaSelecionado){
+    vetor.unshift(number.value);
+    console.log(vetor);
+    console.log(number.value);
+  }
 }
-function remove(){
-   
+function remove() {
+   if(isPilhaSelecionado){
+      vetor.pop()
+   }else if(isFilaSelecionado){
+      vetor.shift()
+   }
+   console.log(vetor)
 }
-function clean(){
-   
+function clean() {
+  for (let i = 0; i <= vetor.length; i++) {
+    vetor.splice(i);
+  }
+  console.log(vetor);
+}
+
+function isPilha() {
+  isPilhaSelecionado = true;
+  isFilaSelecionado = false;
+}
+
+function isFila() {
+  isPilhaSelecionado = false;
+  isFilaSelecionado = true;
 }
