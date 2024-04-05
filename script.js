ed4b63e58b11e390f3b18a7e6393dd649f32564e
lucca milano <100202290+LuccaMilanoDev@users.noreply.github.com>
2024-04-05 12:00:37 +0000

    tentando

---
+++ script.js
@@ -0,0 +1,12 @@
+const vetor = [];
+const number = document.getElementById("number").value
+function add(){
+   vetor.push[2]
+   console.log(vetor[0])
+}
+function remove(){
+   
+}
+function clean(){
+   
+}
